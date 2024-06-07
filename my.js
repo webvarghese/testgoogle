6ad743e69445f09af6afb70e35c23c4f44d2f3ea
Varghese Kurian <76741162+webvarghese@users.noreply.github.com>
2024-06-07 22:19:17 +0530

    Add files via upload

---
+++ my.js
@@ -0,0 +1,12 @@
+function testGS() {
+  const url =
+    "https://script.google.com/macros/s/AKfycbxaM4-WD_wuTwbxJ1F-C8K2BiGU_xNoUVDtgw86LPm-uaH6JPkx1W_32HOhlhBmOAw/exec"
+
+  fetch(url)
+    .then((d) => d.json())
+    .then((d) => {
+      document.getElementById("app").textContent = d[0].status
+    })
+}
+
+document.getElementById("btn").addEventListener("click", testGS)
