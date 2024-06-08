8e062df38a33b4f81871fb94b09db119e1006a31
Varghese Kurian <varghesekurian04@gmail.com>
2024-06-08 18:10:34 +0530

    change code

--- my.js
+++ my.js
@@ -1,5 +1,5 @@
 const url =
-  "https://script.google.com/macros/s/AKfycbxcc1X_283QieqSZzkI7uvm5SY2t_sno_saLFDVyxJ_dYBNhT9ms859GwaXmpvN2NcU/exec"
+  "https://script.google.com/macros/s/AKfycbxh4eQuPJU9gOk62ZOHaUIzKRzqaCsNGzPUc6wRRR-K6mf6Qe3JSFuiftZPdud9k8f1/exec"
 function testGS() {
   // const url =
   //   "https://script.google.com/macros/s/AKfycbxaM4-WD_wuTwbxJ1F-C8K2BiGU_xNoUVDtgw86LPm-uaH6JPkx1W_32HOhlhBmOAw/exec"
@@ -18,15 +18,12 @@ function addGS() {
   fetch(url, {
     method: "POST", // *GET, POST, PUT, DELETE, etc.
     mode: "no-cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "omit", // include, *same-origin, omit
     headers: {
       "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
     },
     redirect: "follow", // manual, *follow, error
-    // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify({ name: "Olivia" }), // body data type must match "Content-Type" header
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify({ name: "John" }), // body data type must match "Content-Type" header
   })
 }
 //made the branch main
